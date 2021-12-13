dc53b170c2c433873747677c56b6e9c0579361d7
Nikita Shinov <shinov_96@mail.ru>
2021-12-14 02:04:54 +0300

    PersistenceManager skeleton added

--- StocksApp/Managers/PersistenceManager.swift
+++ StocksApp/Managers/PersistenceManager.swift
@@ -11,7 +11,27 @@ import Foundation
 final class PersistenceManager {
     static let shared = PersistenceManager()
     
+    private let userDefaults: UserDefaults = .standard
+    
+    private struct Constants {
+        
+    }
+    
     private init () {}
     
+    public var watchList: [String] {
+        return []
+    }
     
+    public func addToWatchList() {
+        
+    }
+    
+    public func removeFromWatchList() {
+        
+    }
+    
+    private var hasOnboarded: Bool {
+        return false
+    }
 }
